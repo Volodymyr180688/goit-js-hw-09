cf4263c2e8872274dc64916d2202ae7d0898239e
Volodymyr180688 <shurik180688@gmail.com>
2024-03-09 17:09:18 +0200

    Homework #9

---
+++ src/js/2-form.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('.feedback-form');
+  const emailInput = form.querySelector('input[name="email"]');
+  const messageInput = form.querySelector('textarea[name="message"]');
+  const formStateKey = "feedback-form-state";
+
+   function saveFormState() {
+    const formState = {
+      email: emailInput.value.trim(),
+      message: messageInput.value.trim()
+    };
+    localStorage.setItem(formStateKey, JSON.stringify(formState));
+  }
+
+
+    function loadFormState() {
+    const savedState = localStorage.getItem(formStateKey);
+    if (savedState) {
+      const parsedState = JSON.parse(savedState);
+      emailInput.value = parsedState.email;
+      messageInput.value = parsedState.message;
+    }
+    }
+    
+
+    loadFormState();
+    
+
+    form.addEventListener('input', saveFormState);
+    
+
+    form.addEventListener('submit', function (event) {
+      
+    const emailValue = emailInput.value.trim();
+    const messageValue = messageInput.value.trim();
+      if (emailValue !== '' && messageValue !== '') { 
+        
+      const formData = {
+        email: emailValue,
+        message: messageValue
+      };
+      console.log(formData);
+      localStorage.removeItem(formStateKey);
+      emailInput.value = '';
+      messageInput.value = '';
+    }
+  });
+});
